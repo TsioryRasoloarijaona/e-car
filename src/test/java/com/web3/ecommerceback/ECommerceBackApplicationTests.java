643ad3c74a8f571083b441ba442ce6537d6c2d8c
Tsiory <hei.tsiory@gmail.com>
2024-05-27 10:37:39 +0300

    project initialisation

---
+++ src/test/java/com/web3/ecommerceback/ECommerceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web3.ecommerceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
