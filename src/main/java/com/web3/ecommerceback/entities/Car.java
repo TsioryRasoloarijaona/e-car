d48fc3c21a73b089d214ee5598f5b652fa01729a
Tsiory <hei.tsiory@gmail.com>
2024-05-28 13:44:55 +0300

    refractor: DBconfig / chore: creating table car

---
+++ src/main/java/com/web3/ecommerceback/entities/Car.java
@@ -0,0 +1,48 @@
+package com.web3.ecommerceback.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "car")
+public class Car implements Serializable {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.SEQUENCE)
+
+    private long id ;
+   @Column(nullable = false , length = 100)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(name = "motor_type" , nullable = false)
+    private String motorType;
+
+    @Column(nullable = false)
+    private int power;
+
+    @Column(name = "place_number" , nullable = false)
+    private int placeNumber;
+
+    @Column(nullable = false , columnDefinition = "boolean default false")
+    private boolean status; //pinned or not
+
+    private String type;
+}
