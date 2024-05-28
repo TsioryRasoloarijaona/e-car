b229b82ebc11264d7798f80d9f2771878be511c4
Tsiory <hei.tsiory@gmail.com>
2024-05-28 22:15:31 +0300

    feat: send an email when validate or reject an appointment

--- src/main/java/com/web3/ecommerceback/service/AppointementService.java
+++ src/main/java/com/web3/ecommerceback/service/AppointementService.java
@@ -15,15 +15,15 @@ public class AppointementService {
     private MailSender mailSender;
 
     public String appointement(Appointment appointement) {
-        try{
-            String subject = "Appointment request - "+appointement.getAppointmentDate()+" "+appointement.getEmail();
+        try {
+            String subject = "Appointment request - " + appointement.getAppointmentDate() + " " + appointement.getEmail();
             String body = appointement.getMessage();
-            boolean sent = mailSender.sendMail(System.getenv("ADMIN_MAIL"),subject,body);
-            if(sent){
+            boolean sent = mailSender.sendMail(System.getenv("ADMIN_MAIL"), subject, body);
+            if (sent) {
                 repository.save(appointement);
             }
             return "your request has been sent";
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return "failed to appointment";
         }
@@ -36,12 +36,19 @@ public class AppointementService {
                     appointment.get()
                             .getAppointmentDate()
                             .toLocalDate().isAfter(LocalDate.now())) {
-                repository.updateStatus("validated",id);
-                return "appointment validated";
+                String subject = "Appointment request - " + appointment.get().getAppointmentDate();
+                String body = "mr/mrs " + appointment.get().getFirstName() + " we are please to announce you that your appointment request has been validated " +
+                        "see you on " + appointment.get().getAppointmentDate();
+                boolean mail = mailSender.sendMail(appointment.get().getEmail(), subject, body);
+                if (mail) {
+                    repository.updateStatus("validated", id);
+                    return "appointment validated";
+                }
+
             }
 
             return "failed to validate appointment";
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return "failed to validate appointment";
         }
@@ -49,9 +56,19 @@ public class AppointementService {
 
     public String rejectAppointment(long id) {
         try {
-            repository.updateStatus("rejected",id);
-            return "appointment rejected";
-        }catch (Exception e){
+            Optional<Appointment> appointment = repository.findById(id);
+            if (appointment.isPresent()) {
+                String subject = "Appointment request - " + appointment.get().getAppointmentDate();
+                String body = "mr/mrs " + appointment.get().getFirstName() + " we are sorry to announce you that your appointment request has been rejected ";
+                boolean mail = mailSender.sendMail(appointment.get().getEmail(), subject, body);
+                if (mail) {
+                    repository.updateStatus("rejected", id);
+                    return "appointment rejected";
+                }
+
+            }
+            return "failed to reject appointment";
+        } catch (Exception e) {
             e.printStackTrace();
             return "failed to reject appointment";
         }
@@ -59,9 +76,9 @@ public class AppointementService {
 
     public String archiveAppointment(long id) {
         try {
-            repository.updateStatus("archived",id);
+            repository.updateStatus("archived", id);
             return "appointment archived";
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return "failed to archive appointment";
         }
